549f68fdc1c4bdca229fa29a2f370a6f20381e12
Coder Duck <amazinglycoolotter2838@gmail.com>
2025-08-23 20:55:10 -0400

    yaysdghdsd

---
+++ beginning/chant.js
@@ -0,0 +1,14 @@
+let decision;
+
+document.getElementById("myAnswer").onclick = function(){
+
+    decision = document.getElementById("myAnswer").value;
+    if 
+
+}
+const chant = document.createElement("h1")
+newH1.textContent="Something happens"
+document.body.prepend(newH1)
+
+
+
