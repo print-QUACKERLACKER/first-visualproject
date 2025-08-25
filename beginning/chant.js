d848cef3e79edeae6c79ae86126b9ef31a409bb8
Coder Duck <amazinglycoolotter2838@gmail.com>
2025-08-24 23:56:13 -0400

    js works

--- beginning/chant.js
+++ beginning/chant.js
@@ -1,14 +1,23 @@
-let decision;
+let decision ;
 
 document.getElementById("myAnswer").onclick = function(){
 
-    decision = document.getElementById("myAnswer").value;
-    if 
+    decision = document.getElementById("chantAnswer").value;
+    //document.getElementById("chantAnswer").value = 'hi'
+
+    if (decision === 'hi') {
+        
+        window.location.href = 'theotherside.html'
+    }
+    else{
+        const newH1 = document.createElement("h1")
+        newH1.textContent="Nothing happens."
+        document.body.prepend(newH1)
+
+    }
 
 }
-const chant = document.createElement("h1")
-newH1.textContent="Something happens"
-document.body.prepend(newH1)
+
 
 
 
